db8cc4a98d4fff3acf71d304519d0662b8b85d23
ThePythonEnvy <63149343+ThePythonEnvy@users.noreply.github.com>
2020-04-04 05:41:14 -0700

    requested user input and casted user string into int produced two variables by multiplying different floats printed the two variables within a continuous string

--- planets.py
+++ planets.py
@@ -1,6 +1,9 @@
 def weight_on_planets():
    # write your code here
-   
+   eweight = int(input("What do you weigh on earth? "))
+   mweight = eweight * 0.38
+   jweight = eweight * 2.34
+   print(f"\nOn Mars you would weigh {mweight} pounds.\nOn Jupiter you would weigh {jweight} pounds.")
    
    
 if __name__ == '__main__':
